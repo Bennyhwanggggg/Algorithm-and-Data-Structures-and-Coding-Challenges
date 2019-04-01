84d6cd233c157631c0c251385ab6dad4707d2dc9
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-01 22:28:37 +1100

    more update

--- Challenges/ValidParantheses.py
+++ Challenges/ValidParantheses.py
@@ -20,3 +20,23 @@ class Solution:
         if not stack:
             return True
         return False
+
+
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        closing = {'}': '{', ']': '[', ')': '('}
+        stack = []
+        for b in s:
+            if b not in closing:
+                stack.append(b)
+            else:
+                if not stack:
+                    return False
+                opening = stack.pop()
+                if closing[b] != opening:
+                    return False
+        return False if stack else True
---
+++ Challenges/wiggleSortII.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        half = len(nums[::2]) - 1
+        nums[::2], nums[1::2] = nums[half::-1], nums[:half:-1]
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        sorted_nums = sorted(nums)
+        for i in list(range(1, len(nums), 2)) + list(range(0, len(nums), 2)) :
+            nums[i] = sorted_nums.pop()
