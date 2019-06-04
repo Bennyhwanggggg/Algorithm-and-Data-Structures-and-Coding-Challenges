89fac58324b57ef0f30b7f12c35cb46788d2ff08
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-04 14:24:22 +1000

    add height checker

---
+++ Challenges/HeightChecker.py
@@ -0,0 +1,29 @@
+"""
+Students are asked to stand in non-decreasing order of heights for an annual photo.
+
+Return the minimum number of students not standing in the right positions.  (This is the number of students that must move in order for all students to be standing in non-decreasing order of height.)
+
+ 
+
+Example 1:
+
+Input: [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+Students with heights 4, 3 and the last 1 are not standing in the right positions.
+ 
+
+Note:
+
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
+"""
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        ordered = sorted(heights)
+        diff = 0
+        for a, b in zip(heights, ordered):
+            if a != b:
+                diff += 1
+        return diff
