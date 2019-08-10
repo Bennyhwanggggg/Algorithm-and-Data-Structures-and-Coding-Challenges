fa9ffc9279b5eef4ef54d0619da8d90e5244ae71
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-08-10 13:40:43 +1000

    update old easy

--- Challenges/findCommonCharacters.py
+++ Challenges/findCommonCharacters.py
@@ -1,3 +1,55 @@
+"""
+Find Common Characters
+
+Given an array A of strings made only from lowercase letters, return a list of all characters that show up in all strings within the list (including duplicates).  For example, if a character occurs 3 times in all strings but not 4 times, you need to include that character three times in the final answer.
+
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: ["cool","lock","cook"]
+Output: ["c","o"]
+"""
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        check = set(A[0])
+        result = [[l] * min([a.count(l) for a in A]) for l in check]
+        return sorted([i for e in result for i in e])
+    
+from collections import Counter
+
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        my_list, res = [], []
+        for v in A:
+            my_list.append(Counter(v))
+        for key in my_list[0]:
+            times = my_list[0][key]
+            for e in my_list[1:]:
+                times = min(times, e[key])
+            for _ in range(times):
+                res.append(key)
+        return res
+    
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        output = []
+        for letter in set(min(A)):
+            count = 100
+            for word in A:
+                count = min(count, word.count(letter))
+            while count > 0:
+                count -= 1
+                output.append(letter)
+        return output
+
+
 class Solution:
     def commonChars(self, A: List[str]) -> List[str]:
         if not A:
--- Challenges/sortArrayByParityII.py
+++ Challenges/sortArrayByParityII.py
@@ -1,3 +1,65 @@
+"""
+Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
+
+Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
+
+You may return any answer array that satisfies this condition.
+
+
+Example 1:
+
+Input: [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+Note:
+
+2 <= A.length <= 20000
+A.length % 2 == 0
+0 <= A[i] <= 1000
+
+"""
+class Solution:
+    def sortArrayByParityII(self, A: List[int]) -> List[int]:
+        res = []
+        odd, even = [], []
+        for n in A:
+            if n%2:
+                odd.append(n)
+            else:
+                even.append(n)
+        
+        for o, e in zip(odd, even):
+            res.append(e)
+            res.append(o)
+        return res
+
+"""
+In place
+First, it is enough to put all even elements in the correct place, since all odd elements will be in the correct place too. So let's only focus on A[0], A[2], A[4], ...
+
+Ideally, we would like to have some partition where everything to the left is already correct, and everything to the right is undecided.
+
+Indeed, this idea works if we separate it into two slices even = A[0], A[2], A[4], ... and odd = A[1], A[3], A[5], .... Our invariant will be that everything less than i in the even slice is correct, and everything less than j in the odd slice is correct.
+
+Algorithm
+
+For each even i, let's make A[i] even. To do it, we will draft an element from the odd slice. We pass j through the odd slice until we find an even element, then swap. Our invariant is maintained, so the algorithm is correct.
+
+Time: O(N)
+Space: O(1)
+"""
+class Solution(object):
+    def sortArrayByParityII(self, A):
+        j = 1
+        for i in xrange(0, len(A), 2):
+            if A[i] % 2:
+                while A[j] % 2:
+                    j += 2
+                A[i], A[j] = A[j], A[i]
+        return A
+
+
 class Solution:
     def sortArrayByParityII(self, A: List[int]) -> List[int]:
         odd, even = [], []
