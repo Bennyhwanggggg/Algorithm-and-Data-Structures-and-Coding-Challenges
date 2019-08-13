b25cbecf817d4976ceee3502e1ec99cac3c0fa96
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-08-13 23:01:31 +1000

    update small

---
+++ Challenges/binaryTreePaths.py
@@ -0,0 +1,108 @@
+"""
+Binary Tree Paths
+
+Given a binary tree, return all root-to-leaf paths.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+Output: ["1->2->5", "1->3"]
+
+Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+"""
+Recursion
+Time: O(N)
+Space: O(N)
+"""
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        if not root:
+            return []
+        if not root.left and not root.right:
+            return [str(root.val)]
+        return [str(root.val) + '->' + i for i in self.binaryTreePaths(root.left)] + [str(root.val) + '->' + i for i in self.binaryTreePaths(root.right)] 
+    
+
+class Solution:
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        def construct_paths(root, path):
+            if root:
+                path += str(root.val)
+                if not root.left and not root.right:  # if reach a leaf
+                    paths.append(path)  # update paths  
+                else:
+                    path += '->'  # extend the current path
+                    construct_paths(root.left, path)
+                    construct_paths(root.right, path)
+
+        paths = []
+        construct_paths(root, '')
+        return paths
+    
+"""
+Iterative
+"""
+class Solution:
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if not root:
+            return []
+        
+        paths = []
+        stack = [(root, str(root.val))]
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right:
+                paths.append(path)
+            if node.left:
+                stack.append((node.left, path + '->' + str(node.left.val)))
+            if node.right:
+                stack.append((node.right, path + '->' + str(node.right.val)))
+        
+        return paths
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+
+        if not root:
+            return []
+        if not root.left and not root.right:
+            return [str(root.val)]
+        return [str(root.val) + "->" + i for i in self.binaryTreePaths(root.left)] + [str(root.val) + "->" + i for i in
+                                                                                      self.binaryTreePaths(root.right)]
