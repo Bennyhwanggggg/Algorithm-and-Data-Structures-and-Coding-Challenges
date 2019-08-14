ba4a70ac29a53c87f1f22b9d4d9ee8c277f84627
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-08-14 22:00:55 +1000

    update 2

---
+++ Challenges/reorderLogFiles.py
@@ -0,0 +1,38 @@
+"""
+Reorder Log files
+
+You have an array of logs.  Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+
+Each word after the identifier will consist only of lowercase letters, or;
+Each word after the identifier will consist only of digits.
+We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+ 
+
+Example 1:
+
+Input: ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+"""
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        digits = []
+        letters = []
+		# divide logs into two parts, one is digit logs, the other is letter logs
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+                
+        letters.sort(key = lambda x: x.split()[0])  # when suffix is tie, sort by identifier
+        letters.sort(key = lambda x: x.split()[1:])  # sort by suffix
+        result = letters + digits  # put digit logs after letter logs
+        return result
+
