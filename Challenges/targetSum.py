ad6c817306b5ed7e3b91e3334801ec39ed1af234
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-08-17 10:27:15 +1000

    add new

---
+++ Challenges/targetSum.py
@@ -0,0 +1,64 @@
+"""
+Target Sum
+
+You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+
+Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+Example 1:
+Input: nums is [1, 1, 1, 1, 1], S is 3. 
+Output: 5
+Explanation: 
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+
+There are 5 ways to assign symbols to make the sum of nums be target 3.
+Note:
+The length of the given array is positive and will not exceed 20.
+The sum of elements in the given array will not exceed 1000.
+Your output answer is guaranteed to be fitted in a 32-bit integer.
+"""
+
+"""
+Backtracking with memo
+
+Time: O(2^n)? <- without memo or O(ln) where l is the range of sum and n is the size of array
+Space: O(n)
+"""
+class Solution:
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        
+        def findTarget(i, s):
+            if (i, s) not in cache:
+                r = 0
+                if i == len(nums) and s == 0:
+                    r = 1
+                elif i != len(nums):
+                    r = findTarget(i+1, s-nums[i]) + findTarget(i+1, s+nums[i])
+                cache[(i, s)] = r
+            return cache[(i, s)]
+        
+        cache = {}
+        return findTarget(0, S)
+
+"""
+DP
+Time: O(N)
+Space: O(N)
+"""
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        if not nums:
+            return 0
+        dic = {nums[0]: 1, -nums[0]: 1} if nums[0] != 0 else {0: 2}
+        for i in range(1, len(nums)):
+            tdic = {}
+            for d in dic:
+                tdic[d + nums[i]] = tdic.get(d + nums[i], 0) + dic.get(d, 0)
+                tdic[d - nums[i]] = tdic.get(d - nums[i], 0) + dic.get(d, 0)
+            dic = tdic
+        return dic.get(S, 0)
