5dae0ac9d21f974aa71d4c84f0538a6fdec96bba
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-11-02 18:29:45 +1100

    update 3 02/11/2019

---
+++ Challenges/favouriteGeneres.py
@@ -0,0 +1,77 @@
+"""
+Given a map Map<String, List<String>> userSongs with user names as keys and a list of all the songs that the user has listened to as values.
+
+Also given a map Map<String, List<String>> songGenres, with song genre as keys and a list of all the songs within that genre as values. The song can only belong to only one genre.
+
+The task is to return a map Map<String, List<String>>, where the key is a user name and the value is a list of the user's favorite genre(s). Favorite genre is the most listened to genre. A user can have more than one favorite genre if he/she has listened to the same number of songs per each of the genres.
+
+Example 1:
+
+Input:
+userSongs = {  
+   "David": ["song1", "song2", "song3", "song4", "song8"],
+   "Emma":  ["song5", "song6", "song7"]
+},
+songGenres = {  
+   "Rock":    ["song1", "song3"],
+   "Dubstep": ["song7"],
+   "Techno":  ["song2", "song4"],
+   "Pop":     ["song5", "song6"],
+   "Jazz":    ["song8", "song9"]
+}
+
+Output: {  
+   "David": ["Rock", "Techno"],
+   "Emma":  ["Pop"]
+}
+
+Explanation:
+David has 2 Rock, 2 Techno and 1 Jazz song. So he has 2 favorite genres.
+Emma has 2 Pop and 1 Dubstep song. Pop is Emma's favorite genre.
+Example 2:
+
+Input:
+userSongs = {  
+   "David": ["song1", "song2"],
+   "Emma":  ["song3", "song4"]
+},
+songGenres = {}
+
+Output: {  
+   "David": [],
+   "Emma":  []
+}
+"""
+
+def initialize():
+    userSongs = {  
+       "David": ["song1", "song2", "song3", "song4", "song8"],
+       "Emma":  ["song5", "song6", "song7"]
+    }
+    songGenres = {  
+       "Rock":    ["song1", "song3"],
+       "Dubstep": ["song7"],
+       "Techno":  ["song2", "song4"],
+       "Pop":     ["song5", "song6"],
+       "Jazz":    ["song8", "song9"]
+    }
+    return userSongs, songGenres
+
+def favGenres(userSongs, songGenres):
+    output = {}
+    for user in userSongs:
+        song_list = userSongs[user]
+        count = {}
+
+        for song in song_list:
+            for genre in songGenres:
+                if(song in songGenres[genre]):
+                    count[genre] = count.get(genre,0) + 1
+
+        output[user] = [key for key, val in count.items() if val == max(count.values())]
+    
+    return output
+
+if __name__ == '__main__':
+    userSongs, songGenres = initialize()
+    print(favGenres(userSongs, songGenres))
